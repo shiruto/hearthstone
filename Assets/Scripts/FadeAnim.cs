4f58ed3e18e720124a83f4166edf581811dc568f
shiruto <shiruto643@gmail.com>
2023-02-28 12:22:34 +0800

    first

---
+++ Assets/Scripts/FadeAnim.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class FadeAni: MonoBehaviour {
+	// Start is called before the first frame update
+
+	TextMeshProUGUI text;
+	[VectorLabels("From", "To")]
+	public Vector2 TransparencyChange = new(0, 1);
+	public bool isLooping = true;
+	public float fadeSpeed = 0.1f;
+	bool isIncresing;
+	bool isWorking = true;
+	public float delay = 0.0f;
+	Color tempColor;
+	float startTime;
+	float curTime;
+
+	void Start() {
+		text = this.GetComponent<TextMeshProUGUI>();
+		isIncresing = TransparencyChange.x < TransparencyChange.y;
+		tempColor = text.color;
+		tempColor.a = TransparencyChange.x;
+		text.color = tempColor;
+		startTime = Time.time;
+		//Debug.Log("Start: " + text.color.a);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		curTime = Time.time;
+		if (curTime - startTime - delay > 0.01f) {
+			if(isWorking) {
+				text.color = changeTransparency(text.color);
+			}
+		}
+		//Debug.Log("outside of func: \ntext color: " + text.color.a + "\ttemp color: " + tempColor.a);
+	}
+
+	private Color changeTransparency(Color color) {
+		if(isIncresing) {
+			color.a += fadeSpeed * Time.deltaTime;
+			if(color.a >= Mathf.Max(TransparencyChange.x, TransparencyChange.y)) {
+				isIncresing = false;
+			}
+		}
+		else {
+			color.a -= fadeSpeed * Time.deltaTime;
+			if(color.a <= Mathf.Min(TransparencyChange.x, TransparencyChange.y)) {
+				isIncresing = true;
+			}
+		}
+		if (!isLooping && Mathf.Abs(color.a - TransparencyChange.y) < 0.01f) {
+			isWorking = false;
+		}
+		Debug.Log("isWorking = "+ isWorking + "\tisIncresing = " + isIncresing + "\ttransparency: "+ color.a);
+		return color;
+	}
+}
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class StartMenu: MonoBehaviour {
+	// Start is called before the first frame update
+	public GameObject canvas;
+
+	void Start() {
+		Application.targetFrameRate = 60;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
